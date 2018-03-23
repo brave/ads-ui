fb9a7cb6acb6237ceaad6caefb2766efb746b675
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2018-03-23 18:10:12 -0400

    init new creative page

---
+++ src/components/Creatives/CreativesNew/CreativesNew.style.ts
@@ -0,0 +1,8 @@
+export const style = {
+  card: {
+    margin: "20px",
+  },
+  root: {
+    width: "100%",
+  },
+};
--- src/components/Creatives/CreativesNew/CreativesNew.tsx
+++ src/components/Creatives/CreativesNew/CreativesNew.tsx
@@ -1,11 +1,28 @@
+import { AppBar, Card, CardContent, Toolbar, Typography, withStyles } from "material-ui";
 import * as React from "react";
+
+import CreativeForm from "../CreativeForm/CreativeForm";
+
+import { style } from "./CreativesNew.style";
 
 class CreativesNew extends React.Component<any, any> {
   public render() {
+    const { classes } = this.props;
     return (
-      <div>new creative</div>
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title">New Creative</Typography>
+          </Toolbar>
+        </AppBar>
+        <Card className={classes.card}>
+          <CardContent>
+            <CreativeForm />
+          </CardContent>
+        </Card>
+      </div>
     );
   }
 }
 
-export default CreativesNew;
+export default withStyles(style)(CreativesNew);
