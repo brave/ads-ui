40bcf974eee5692aae53e0c0c266b3239e3d7a72
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2018-09-22 19:29:18 -0400

    startat and endat of campaign

--- src/components/Campaigns/CampaignForm/CampaignForm.tsx
+++ src/components/Campaigns/CampaignForm/CampaignForm.tsx
@@ -2,7 +2,7 @@ import { Button, withStyles } from "@material-ui/core";
 import * as React from "react";
 import { Field, initialize, reduxForm } from "redux-form";
 
-import { renderTextField } from "../../../containers/field-material";
+import { renderDateField, renderTextField } from "../../../containers/field-material";
 
 import { styles } from "./CampaignForm.style";
 
@@ -28,6 +28,22 @@ class CampaignForm extends React.Component<any, any> {
           <div>
             <Field className={classes.textField} disabled={!unlock}
               name="name" type="text" component={renderTextField} label="Name" />
+          </div>
+          <div>
+            <Field className={classes.textField} disabled={!unlock}
+              name="budget" type="number" component={renderTextField} label="Budget" />
+          </div>
+          <div>
+            <Field className={classes.textField} disabled={!unlock}
+              name="dailyCap" type="number" component={renderTextField} label="Daily Cap" />
+          </div>
+          <div>
+            <Field className={classes.textField} disabled={!unlock}
+             name="startAt" component={renderDateField} label="Start Date"/>
+          </div>
+          <div>
+            <Field className={classes.textField} disabled={!unlock}
+             name="endAt" component={renderDateField} label="End Date"/>
           </div>
           {unlock &&
             <div>
