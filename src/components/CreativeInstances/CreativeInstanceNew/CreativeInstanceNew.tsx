ff17eddabf3e70c955dab0641f9ee342618fd749
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2019-05-15 11:05:06 -0400

    #84 fix

--- src/components/CreativeInstances/CreativeInstanceNew/CreativeInstanceNew.tsx
+++ src/components/CreativeInstances/CreativeInstanceNew/CreativeInstanceNew.tsx
@@ -1,5 +1,6 @@
 import { AppBar, Card, CardContent, Toolbar, Typography, withStyles } from "@material-ui/core";
 import React from "react";
+import * as _ from "lodash";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
@@ -9,20 +10,24 @@ import CreativeInstanceForm from "../CreativeInstanceForm/CreativeInstanceForm";
 import { styles } from "./CreativeInstanceNew.style";
 
 class CreativeInstanceNew extends React.Component<any, any> {
-  public componentDidMount(){
+  public componentDidMount() {
     this.props.getCreatives(this.props.auth, this.props.match.params.userId);
   }
   public render() {
     const { classes, create, auth, history, match, confirmationTypes, creatives } = this.props;
+    let advertiserCreatives = _.cloneDeep(creatives);
+    if (this.props.match.params.advertiserId) {
+      advertiserCreatives = _.filter(advertiserCreatives, { advertiserId: this.props.match.params.advertiserId })
+    }
     const handleSubmit = async (value: any) => {
       console.log(value);
-      if(value){
-        if(value.prices && value.prices.length > 0){
-          for(const price of value.prices){
+      if (value) {
+        if (value.prices && value.prices.length > 0) {
+          for (const price of value.prices) {
             value.amount = Number(value).valueOf();
           }
         }
-        if(value.creative){
+        if (value.creative) {
           value.creativeId = value.creative.id;
         }
       }
@@ -40,7 +45,7 @@ class CreativeInstanceNew extends React.Component<any, any> {
         </AppBar>
         <Card className={classes.card}>
           <CardContent>
-            <CreativeInstanceForm confirmationTypes={confirmationTypes} creatives={creatives} unlock={true} onSubmit={handleSubmit} />
+            <CreativeInstanceForm confirmationTypes={confirmationTypes} creatives={advertiserCreatives} unlock={true} onSubmit={handleSubmit} />
           </CardContent>
         </Card>
       </div>
@@ -56,7 +61,7 @@ const mapStateToProps = (state: any, ownProps: any) => ({
 
 const mapDispatchToProps = (dispatch: any, ownProps: any) => ({
   create: (value: any, auth: any) => dispatch((CreateCreativeInstances(value, auth))),
-  getCreatives: (auth: any, userId: string)=>dispatch((GetCreatives(auth, userId)))
+  getCreatives: (auth: any, userId: string) => dispatch((GetCreatives(auth, userId)))
 });
 
 export default withRouter(withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(CreativeInstanceNew)) as any);
