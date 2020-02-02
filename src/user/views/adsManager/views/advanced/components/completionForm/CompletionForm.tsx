c44ca964cdd2ec02c3f88516abb4a401ef655e0b
Dan A. Lipeles <danlipeles@icloud.com>
2020-02-02 00:02:07 -0500

    Port campaign creator (#229)
    * Commit
    
    * Commit

---
+++ src/user/views/adsManager/views/advanced/components/completionForm/CompletionForm.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Context from "../../../../../../../state/context";
+import Confetti from 'react-confetti'
+
+import { createAd, createAdSet, createCampaign, prepareCreateCampaignInput, prepareCreateAdInput, prepareCreateAdSetsInput } from "./lib/CompletionFormLibrary";
+import Section from '../../../../../../../components/section/Section';
+import { Text } from "../../../../../../../components/Text/Text";
+import present from "./assets/present.png";
+import { Link } from 'react-router-dom';
+
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { submitOrder } from './lib/CompletionForm.library';
+
+const linkStyle = { textDecoration: "none", color: "inherit" };
+
+class CompletionForm extends Component<any, any> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            saving: undefined
+        }
+    }
+    public componentDidMount() {
+        this.initialize();
+    }
+
+    public async initialize() {
+        // let createCampaignInput = prepareCreateCampaignInput(this.props.userId, this.props.advertiserId, this.props.campaign, this.props.adSets);
+        // let createCampaignResponse = await createCampaign(createCampaignInput, this.props.auth.accessToken);
+        submitOrder(this.props.userId, this.props.advertiserId, this.props.campaign, this.props.adSets, this.props.auth.accessToken);
+    }
+
+    render() {
+        return (
+            <div style={{ marginLeft: "-255px" }}>
+                <Confetti style={{ top: "-62px" }} colors={["#FB7959", "#4C54D2"]} />
+                <div style={{ width: "843px", height: "100%", marginLeft: "auto", marginRight: "auto", display: "flex", alignItems: "center" }}>
+                    <Section style={{ marginTop: "auto", marginBottom: "auto" }} fullWidthChild={true}>
+                        <div style={{ zIndex: 5, backgroundColor: "white" }}>
+                            <div style={{ width: '100%', display: "flex", justifyContent: "center", alignItems: "center", flexDirection: 'column' }}>
+                                <img src={present} style={{ width: "400px", marginLeft: "auto", marginRight: "auto" }} />
+                                <Text content={"Congratulations!"} sizes={[16, 16, 15, 15, 28]} fontFamily={"Poppins"} />
+                                <div style={{ width: "75%" }}>
+                                    <Text style={{ textAlign: "center", marginTop: "8px" }} content={"Your campaign has been created and is now being reviewed by our ads team, we'll send you an e-mail as soon as your campaign is approved and activated. Thank you for using Brave Ads!"} sizes={[16, 16, 15, 15, 15]} fontFamily={"Poppins"} />
+                                </div>
+                                <Link style={linkStyle} to={"/admin/main/dashboard"}>
+                                    <div onClick={() => { }} style={{ display: "flex", justifyContent: "center", padding: "0px 20px", width: "100px", background: "#4C54D2", color: "white", border: "none", borderRadius: "100px 100px 100px 100px", marginTop: "16px", cursor: "pointer" }}>
+                                        <span>
+                                            <Text style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 14]} fontWeight={500} fontFamily={"Poppins"}>
+                                                Done
+                                        </Text>
+                                        </span>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </Section>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CompletionForm;
---
+++ src/user/views/adsManager/views/advanced/components/formProgress/FormProgress.tsx
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import { Text } from "../../../../../../../components/Text/Text";
+import { Icon } from '@material-ui/core';
+import * as S from "./FormProgress.style";
+
+class FormProgress extends Component<any, any> {
+
+    renderOrderTypeIcon() {
+        return (
+            <React.Fragment>
+                <S.InactiveIcon />
+                <Text style={{ marginLeft: "12px", opacity: .5 }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                    Order Type
+                </Text>
+                <S.Line />
+            </React.Fragment>
+        )
+    }
+
+    renderCampaignIcon() {
+        if (this.props.form === "campaignForm") {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("campaignForm")}>
+                        <S.ActiveIcon />
+                        <Text style={{ marginLeft: "12px" }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Campaign
+                    </Text>
+                        <S.Line />
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+        // else if (this.props.form !== "campaignForm" && validateCampaignForm(this.props.campaign) === 'valid') {
+        //     return (
+        //         <S.NavContainer onClick={() => this.props.setForm("campaignForm")}>
+        //             <S.ValidIcon>
+        //                 <Icon style={{ color: "white" }}>done</Icon>
+        //             </S.ValidIcon>
+        //             <Text style={{ marginLeft: "12px" }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+        //                 Campaign
+        //         </Text>
+        //             <S.Line />
+        //         </S.NavContainer>
+        //     )
+        // }
+        else {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("campaignForm")}>
+                        <S.InactiveIcon />
+                        <Text style={{ marginLeft: "12px", opacity: .5 }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Campaign
+                </Text>
+                        <S.Line />
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+    }
+
+    renderAdSetsIcon() {
+        if (this.props.form === "adSetsForm") {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("adSetsForm")}>
+                        <S.ActiveIcon />
+                        <Text style={{ marginLeft: "12px" }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Ad Sets
+                    </Text>
+                        <S.Line />
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+        // else if (this.props.form !== "adSetsForm" && validateAdSetsForm(this.props.adSets) === 'valid') {
+        //     return (
+        //         <S.NavContainer onClick={() => this.props.setForm("adSetsForm")}>
+        //             <S.ValidIcon>
+        //                 <Icon style={{ color: "white" }}>done</Icon>
+        //             </S.ValidIcon>
+        //             <Text style={{ marginLeft: "12px" }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+        //                 Ad Sets
+        //         </Text>
+        //             <S.Line />
+        //         </S.NavContainer>
+        //     )
+        // }
+        else {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("adSetsForm")}>
+                        <S.InactiveIcon />
+                        <Text style={{ marginLeft: "12px", opacity: .5 }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Ad Sets
+                </Text>
+                        <S.Line />
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+    }
+
+    renderAdsIcon() {
+        if (this.props.form === "adsForm") {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("adsForm")}>
+                        <S.ActiveIcon />
+                        <Text style={{ marginLeft: "12px" }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Ads
+                    </Text>
+                        <S.Line />
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+        // else if (this.props.form !== "adsForm" && validateAdsForm(this.props.ads) === 'valid') {
+        //     return (
+        //         <S.NavContainer onClick={() => this.props.setForm("adsForm")}>
+        //             <S.ValidIcon>
+        //                 <Icon style={{ color: "white" }}>done</Icon>
+        //             </S.ValidIcon>
+        //             <Text style={{ marginLeft: "12px" }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+        //                 Ads
+        //         </Text>
+        //             <S.Line />
+        //         </S.NavContainer>
+        //     )
+        // }
+        else {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("adsForm")}>
+                        <S.InactiveIcon />
+                        <Text style={{ marginLeft: "12px", opacity: .5 }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Ads
+                        </Text>
+                        <S.Line />
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+    }
+
+    renderReviewIcon() {
+        if (this.props.form === "reviewForm") {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("reviewForm")}>
+                        <S.ActiveIcon />
+                        <Text style={{ marginLeft: "12px" }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Review
+                    </Text>
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+        else {
+            return (
+                <React.Fragment>
+                    <S.NavContainer onClick={() => this.props.setForm("reviewForm")}>
+                        <S.InactiveIcon />
+                        <Text style={{ marginLeft: "12px", opacity: .5 }} sizes={[16, 16, 15, 15, 14]} fontFamily={"Muli"}>
+                            Review
+                        </Text>
+                    </S.NavContainer>
+                </React.Fragment>
+            )
+        }
+    }
+
+    render() {
+        return (
+            this.props.form !== 'completionForm' &&
+            <React.Fragment>
+                <S.Container>
+                    {this.renderCampaignIcon()}
+                    {this.renderAdSetsIcon()}
+                    {this.renderAdsIcon()}
+                    {this.renderReviewIcon()}
+                </S.Container>
+            </React.Fragment>
+        );
+    }
+}
+
+export default FormProgress;
