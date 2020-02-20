93ab78adf6a974968b3526b184a5057d033fdafd
Dan A. Lipeles <danlipeles@icloud.com>
2020-02-20 17:20:23 -0500

    [WIP] #245, #220, #730 Brave 1.3 Support & Publisher Ads (#247)
    * Commit
    
    * Commit

---
+++ src/admin/views/creatives/creativeNew/lib/Creative.library.tsx
@@ -0,0 +1,5 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from "apollo-boost";
+import React, { Component } from 'react';
