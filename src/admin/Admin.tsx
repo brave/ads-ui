ba116d172b1abf7c50394110c5f1a1ca5ad90989
Dan A. Lipeles <danlipeles@icloud.com>
2020-03-23 22:28:45 -0400

    Dev (#288)
    * Chart patch (#216)
    
    * Commit
    
    * Commit
    
    * Ui for new fields (#223)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Campaign Creator III (#228)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Port campaign creator (#229)
    
    * Commit
    
    * Commit
    
    * Commit (#238)
    
    * Campaign creator v2.1 (#240)
    
    * Commit
    
    * Commit
    
    * [WIP] #245, #220, #730 Brave 1.3 Support & Publisher Ads (#247)
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Pub ads support (#253)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Commit
    
    * Pub ads support (#256)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Fix Codepipeline (#258)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit (#267)
    
    * Commit (#277)
    
    * Commit (#282)
    
    * Commit (#287)

--- src/admin/Admin.tsx
+++ src/admin/Admin.tsx
@@ -1,9 +1,4 @@
 import {
-    Icon,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
     withStyles
 } from "@material-ui/core";
 import React from "react";
@@ -13,22 +8,17 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import AppBar from "../components/AppBar/AppBar";
 import SideBar from "../components/SideBar/SideBar";
 import Dashboard from "./views/dashboard/Dashboard";
-import CampaignsList from "./views/campaignsList/CampaignsList";
 
-import CampaignNew from "../components/Campaigns/CampaignNew/CampaignNew";
-import CreativeSetNew from "../components/CreativeSets/CreativeSetNew/CreativeSetNew";
 import UserList from "../components/Users/UserList/UserList";
 import UserNew from "../components/Users/UserNew/UserNew";
 import CreativeInstanceNew from "../components/CreativeInstances/CreativeInstanceNew/CreativeInstanceNew";
 import CreativeInstanceView from "../containers/Campaigns/CreativeInstanceView/CreativeInstanceView";
 import CampaignPerformance from "../containers/Campaigns/CampaignPerformance/CampaignPerformance";
 
-import CampaignView from "../containers/Campaigns/CampaignView/CampaignView";
+
 import Creative from "./views/creatives/creative/Creative";
 import CreativeNew from "./views/creatives/creativeNew/CreativeNew";
 import InvoiceView from "../containers/Invoices/InvoicesView/InvoiceView";
-import AdvertiserView from "../containers/Users/AdvertiserView/AdvertiserView";
-
 import AdSet from "./views/adSets/adSet/AdSet";
 import AdSetNew from "./views/adSets/adSetNew/AdSetNew";
 
@@ -58,6 +48,8 @@ import AdSetAds from "./views/adSets/adSetAds/AdSetAds";
 import CampaignListOverview from "./views/campaignsList/campaignListOverview/CampaignListOverview";
 import CampaignListApprovals from "./views/campaignsList/campaignListApprovals/CampaignListApprovals";
 import CampaignListPacing from "./views/campaignsList/campaignListPacing/CampaignListPacing";
+import Campaign from "./views/campaigns/campaign/Campaign";
+import CampaignNew from "./views/campaigns/campaignNew/CampaignNew";
 
 
 class Admin extends React.Component<any, any> {
@@ -147,44 +139,34 @@ class Admin extends React.Component<any, any> {
                                 <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign"} component={AdvertiserCampaigns} />
                                 <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/creative"} component={AdvertiserCreatives} />
                                 <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/invoice"} component={AdvertiserInvoices} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/invoice/:invoiceId"}
-                                    component={InvoiceView} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/new"}
-                                    component={CampaignNew} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId"}
-                                    component={CampaignView} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/report"}
-                                    component={CampaignPerformance} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/analytics/overview"}
-                                    component={Overview} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/analytics/platforms"}
-                                    component={Platforms} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/invoice/:invoiceId"} component={InvoiceView} />
+
+                                {/* /campaign */}
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/new"} component={CampaignNew} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId"} component={Campaign} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/report"} component={CampaignPerformance} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/analytics/overview"} component={Overview} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/analytics/platforms"} component={Platforms} />
 
                                 {/* /creativeSet */}
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/new"}
-                                    component={AdSetNew} />
-                                <Route exact path={
-                                    match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId"}
-                                    component={AdSet} />
-                                <Route exact path={
-                                    match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId/ads"}
-                                    component={AdSetAds} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/new"} component={AdSetNew} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId"} component={AdSet} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId/ads"} component={AdSetAds} />
 
                                 {/* /creativeInstance */}
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId/creativeInstance/new"}
-                                    component={CreativeInstanceNew} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId/creativeInstance/:creativeInstanceId"}
-                                    component={CreativeInstanceView} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId/creativeInstance/new"} component={CreativeInstanceNew} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/campaign/:campaignId/creativeSet/:creativeSetId/creativeInstance/:creativeInstanceId"} component={CreativeInstanceView} />
 
                                 {/* /advertiser/:advertiserId/creative */}
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/creative/new"}
-                                    component={CreativeNew} />
-                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/creative/:creativeId"}
-                                    component={Creative} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/creative/new"} component={CreativeNew} />
+                                <Route exact path={match.url + "/users/:userId/advertiser/:advertiserId/creative/:creativeId"} component={Creative} />
 
+                                {/* /campaigns */}
                                 <Route path={match.url + "/campaigns/overview"} component={CampaignListOverview} />
                                 <Route path={match.url + "/campaigns/pacing"} component={CampaignListPacing} />
                                 <Route path={match.url + "/campaigns/approvals"} component={CampaignListApprovals} />
+
+                                {/* Redirect to dashboard */}
                                 <Redirect to={match.url + "/dashboard"} />
                             </Switch>
                         </S.Main>
--- src/admin/views/campaignsList/campaignListApprovals/CampaignListApprovals.tsx
+++ src/admin/views/campaignsList/campaignListApprovals/CampaignListApprovals.tsx
@@ -15,21 +15,39 @@ import { Link } from "react-router-dom";
 import { Icon } from "@material-ui/core";
 import { SecondaryButton, TextArea } from "../../../../components/formElements/formElements";
 
-import Modal from '../../../../components/modal/Modal';
+import Modal from 'react-modal';
 import ReactDOM from "react-dom";
 import RadioButton from "../../../../components/radioButton/RadioButton";
 
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 5000,
+        borderRadius: "4px",
+        padding: "56px",
+        border: "1px solid #e2e2e2"
+    }
+};
+
 const CampaignListApprovals = props => {
 
+
+    // Approve Campaign Modal State
     const [showApproveCampaignModal, setShowApproveCampaignModal] = useState(false);
+    const [approveCampaignName, setApproveCampaignName] = useState('');
+    const [approveCampaignId, setApproveCampaignId] = useState('');
+
+    // Reject Campaign Modal State
     const [showRejectCampaignModal, setShowRejectCampaignModal] = useState(false);
+    const [rejectCampaignName, setRejectCampaignName] = useState('');
+    const [rejectCampaignId, setRejectCampaignId] = useState('');
     const [rejectionOption, setRejectionOption] = useState('prohibited_category');
     const [rejectionMessage, setRejectionMessage] = useState('');
-
-    console.log(rejectionOption);
-
-    const inputEl = useRef(null);
-    const modalEl = useRef(null);
 
     const { match } = props;
 
@@ -38,6 +56,32 @@ const CampaignListApprovals = props => {
         { label: "Pacing", selected: false, link: match.url.replace("/approvals", "/pacing") },
         { label: "Approvals", selected: true, link: match.url },
     ]
+
+    const openApproveCampaignModal = (approveCampaignName, approveCampaignId) => {
+        setApproveCampaignName(approveCampaignName);
+        setApproveCampaignId(approveCampaignId);
+        setShowApproveCampaignModal(true);
+    }
+
+    const closeApproveCampaignModal = () => {
+        setApproveCampaignName('');
+        setApproveCampaignId('');
+        setShowApproveCampaignModal(false);
+    }
+
+    const openRejectCampaignModal = (rejectCampaignName, rejectCampaignId) => {
+        setRejectCampaignName(rejectCampaignName);
+        setRejectCampaignId(rejectCampaignId);
+        setShowRejectCampaignModal(true);
+    }
+
+    const closeRejectCampaignModal = () => {
+        setRejectCampaignName('');
+        setRejectCampaignId('');
+        setShowRejectCampaignModal(false);
+        setRejectionOption('prohibited_category');
+        setRejectionMessage('');
+    }
 
     const filterCampaigns = (campaigns) => {
         let temp = campaigns;
@@ -48,10 +92,6 @@ const CampaignListApprovals = props => {
 
     const refresh = () => {
         window.location.reload();
-    }
-
-    const removeModal = () => {
-        ReactDOM.unmountComponentAtNode(inputEl.current as any);
     }
 
     const [approveCampaign, { loading: updateNotificationCreativeLoading, error: updateNotificationCreativeError }] =
@@ -92,35 +132,54 @@ const CampaignListApprovals = props => {
             },
         },
         {
+            Header: 'Start Date',
+            accessor: 'startAt',
+            sortDescFirst: true,
+            Cell: (props) => {
+                return new Date(props.row.original.startAt).toLocaleDateString("en-US")
+            },
+        },
+        {
             Header: ' ',
             Cell: (props) => {
                 return <div style={{ display: "flex" }}>
-                    <SecondaryButton onClick={() => { renderApproveCampaignModal(props.row.original.id, props.row.original.name) }} style={{ marginRight: "12px" }}><Text content={"Approve"} style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 12]} fontWeight={500} fontFamily={"Poppins"} /></SecondaryButton>
-                    <SecondaryButton onClick={() => { renderRejectCampaignModal(props.row.original.id, props.row.original.name) }}><Text content={"Reject"} style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 12]} fontWeight={500} fontFamily={"Poppins"} /></SecondaryButton>
+                    <SecondaryButton onClick={() => { openApproveCampaignModal(props.row.original.name, props.row.original.id) }} style={{ marginRight: "12px" }}><Text content={"Approve"} style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 12]} fontWeight={500} fontFamily={"Poppins"} /></SecondaryButton>
+                    <SecondaryButton onClick={() => { openRejectCampaignModal(props.row.original.name, props.row.original.id) }}><Text content={"Reject"} style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 12]} fontWeight={500} fontFamily={"Poppins"} /></SecondaryButton>
                 </div>
             },
         },
 
     ];
 
-    const renderApproveCampaignModal = (campaignId, campaignName) => {
-        ReactDOM.render(
-            <Modal id="modal">
+    if (loading) return <></>;
+
+    console.log(data.campaigns);
+
+    return (
+        <div>
+            <TabSelector config={tabConfig} />
+            <Section fullWidthChild={true}>
+                <Table data={filterCampaigns(data.campaigns)} columns={columns} tableWidth={1094} columnCount={5} />
+            </Section>
+            <Modal
+                isOpen={showApproveCampaignModal}
+                style={customStyles}
+            >
                 <div style={{ width: "500px" }}>
                     <div style={{ display: "flex" }}>
                         <Text content={`Approve this campaign?`} sizes={[16, 16, 15, 15, 22]} color={"#E0694C"} fontFamily={"Poppins"} />
-                        <Icon onClick={(e) => removeModal()} style={{ marginLeft: "auto", color: "grey", cursor: "pointer" }}>clear</Icon>
+                        <Icon onClick={(e) => closeApproveCampaignModal()} style={{ marginLeft: "auto", color: "grey", cursor: "pointer" }}>clear</Icon>
                     </div>
-                    <Text style={{ marginTop: "42px" }} content={`You are attempting to approve ${campaignName}. Approving this campaign will notify the advertiser and add it to the ads catalog.`} sizes={[16, 16, 15, 15, 16]} fontFamily={"Muli"} />
+                    <Text style={{ marginTop: "42px" }} content={`You are attempting to approve ${approveCampaignName}. Approving this campaign will notify the advertiser and add it to the ads catalog.`} sizes={[16, 16, 15, 15, 16]} fontFamily={"Muli"} />
                     <div style={{ display: "flex", width: "100%", marginTop: "42px" }}>
-                        <div onClick={(e) => removeModal()} style={{ marginLeft: "auto", marginRight: "28px", display: "flex", justifyContent: "center", alignItems: "center", padding: "0px 20px", width: "100px", border: "1px solid #e2e2e2", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
+                        <div onClick={(e) => closeApproveCampaignModal()} style={{ marginLeft: "auto", marginRight: "28px", display: "flex", justifyContent: "center", alignItems: "center", padding: "0px 20px", width: "100px", border: "1px solid #e2e2e2", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
                             <span>
                                 <Text style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 14]} fontWeight={500} fontFamily={"Poppins"}>
                                     Cancel
                     </Text>
                             </span>
                         </div>
-                        <div onClick={() => { approveCampaign({ variables: { approveCampaignInput: { campaignId } } }) }} style={{ display: "flex", justifyContent: "center", padding: "0px 20px", width: "100px", background: "#F87454", color: "white", border: "none", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
+                        <div onClick={() => { approveCampaign({ variables: { approveCampaignInput: { campaignId: approveCampaignId } } }) }} style={{ display: "flex", justifyContent: "center", padding: "0px 20px", width: "100px", background: "#F87454", color: "white", border: "none", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
                             <span>
                                 <Text style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 14]} fontWeight={500} fontFamily={"Poppins"}>
                                     Approve
@@ -129,18 +188,17 @@ const CampaignListApprovals = props => {
                         </div>
                     </div>
                 </div>
-            </Modal>, inputEl.current)
-    }
-
-    const renderRejectCampaignModal = (campaignId, campaignName) => {
-        ReactDOM.render(
-            <Modal id="modal">
+            </Modal>
+            <Modal
+                isOpen={showRejectCampaignModal}
+                style={customStyles}
+            >
                 <div style={{ width: "500px" }}>
                     <div style={{ display: "flex" }}>
                         <Text content={`Reject this campaign?`} sizes={[16, 16, 15, 15, 22]} color={"#E0694C"} fontFamily={"Poppins"} />
-                        <Icon onClick={(e) => removeModal()} style={{ marginLeft: "auto", color: "grey", cursor: "pointer" }}>clear</Icon>
+                        <Icon onClick={(e) => closeRejectCampaignModal()} style={{ marginLeft: "auto", color: "grey", cursor: "pointer" }}>clear</Icon>
                     </div>
-                    <Text style={{ marginTop: "42px" }} content={`You are attempting to reject ${campaignName}. Rejecting this campaign will notify the advertiser and add remove it from the approval queue.`} sizes={[16, 16, 15, 15, 16]} fontFamily={"Muli"} />
+                    <Text style={{ marginTop: "42px" }} content={`You are attempting to reject ${rejectCampaignName}. Rejecting this campaign will notify the advertiser and remove it from the approval queue.`} sizes={[16, 16, 15, 15, 16]} fontFamily={"Muli"} />
                     <Text style={{ marginTop: "42px" }} content={`Reason for rejection: `} sizes={[16, 16, 15, 15, 16]} fontFamily={"Muli"} />
                     <div style={{ display: "flex", marginTop: "28px" }}>
                         <RadioButton onClick={() => { setRejectionOption("prohibited_category") }} checked={rejectionOption === 'prohibited_category'} style={{ marginRight: "8px" }} type="radio" name="gender" value="male" />
@@ -163,14 +221,14 @@ const CampaignListApprovals = props => {
 
                     <TextArea placeholder="Enter additional text..." />
                     <div style={{ display: "flex", width: "100%", marginTop: "42px" }}>
-                        <div onClick={(e) => removeModal()} style={{ marginLeft: "auto", marginRight: "28px", display: "flex", justifyContent: "center", alignItems: "center", padding: "0px 20px", width: "100px", border: "1px solid #e2e2e2", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
+                        <div onClick={(e) => closeRejectCampaignModal()} style={{ marginLeft: "auto", marginRight: "28px", display: "flex", justifyContent: "center", alignItems: "center", padding: "0px 20px", width: "100px", border: "1px solid #e2e2e2", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
                             <span>
                                 <Text style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 14]} fontWeight={500} fontFamily={"Poppins"}>
                                     Cancel
                     </Text>
                             </span>
                         </div>
-                        <div onClick={() => { rejectCampaign({ variables: { rejectCampaignInput: { campaignId, option: "invalid_landing_page" } } }) }} style={{ display: "flex", justifyContent: "center", padding: "0px 20px", width: "100px", background: "#F87454", color: "white", border: "none", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
+                        <div onClick={() => { rejectCampaign({ variables: { rejectCampaignInput: { campaignId: rejectCampaignId, option: rejectionOption } } }) }} style={{ display: "flex", justifyContent: "center", padding: "0px 20px", width: "100px", background: "#F87454", color: "white", border: "none", borderRadius: "100px 100px 100px 100px", cursor: "pointer" }}>
                             <span>
                                 <Text style={{ paddingTop: "6px", paddingBottom: "6px" }} sizes={[16, 16, 15, 15, 14]} fontWeight={500} fontFamily={"Poppins"}>
                                     Reject
@@ -179,23 +237,7 @@ const CampaignListApprovals = props => {
                         </div>
                     </div>
                 </div>
-            </Modal>, inputEl.current)
-    }
-
-
-
-
-    if (loading) return <></>;
-
-    console.log(data.campaigns);
-
-    return (
-        <div>
-            <TabSelector config={tabConfig} />
-            <Section fullWidthChild={true}>
-                <Table data={filterCampaigns(data.campaigns)} columns={columns} tableWidth={1094} columnCount={4} />
-            </Section>
-            <div id="hello" ref={inputEl}></div>
+            </Modal>
         </div>
     );
 }
