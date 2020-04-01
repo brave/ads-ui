68411ea1440071e1d66e47a2fd89e0a03948fe3a
Dan A. Lipeles <danlipeles@icloud.com>
2020-03-31 20:30:37 -0400

    Campaigns List for Users  (#325)
    * Commit
    
    * Commit

--- src/user/campaignList/lib/CampaignList.queries.ts
+++ src/user/campaignList/lib/CampaignList.queries.ts
@@ -1,26 +1,22 @@
 import { gql } from "apollo-boost";
 
-export const CAMPAIGN_LIST_OVERVIEW = gql`
-query{
+export const CAMPAIGN_LIST = gql`
+query advertiser($id: String!) {
+advertiser(id: $id){
     campaigns {
-        advertiser{
-            id
-            billingEmail
-            name
-            userAdvertisers{
-            userId
-        }
-        }
         id
         name
         state
+        dailyBudget
         budget
         spent
+        currency
         createdAt
         startAt
         endAt
         currency
         pacingIndex
     }
+}
 }
 `;
