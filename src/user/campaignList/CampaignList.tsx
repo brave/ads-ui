60dbd7382a02318c7a52174dae1cff321aa9f1f3
Dan A. Lipeles <danlipeles@icloud.com>
2020-03-31 22:50:19 -0400

    Dev (#328)
    * Chart patch (#216)
    
    * Commit
    
    * Commit
    
    * Ui for new fields (#223)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Campaign Creator III (#228)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Port campaign creator (#229)
    
    * Commit
    
    * Commit
    
    * Commit (#238)
    
    * Campaign creator v2.1 (#240)
    
    * Commit
    
    * Commit
    
    * [WIP] #245, #220, #730 Brave 1.3 Support & Publisher Ads (#247)
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Pub ads support (#253)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Commit
    
    * Pub ads support (#256)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Fix Codepipeline (#258)
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Delete creativeNew.tsx
    
    * Commit
    
    * Commit
    
    * Commit
    
    * Commit (#267)
    
    * Commit (#277)
    
    * Commit (#282)
    
    * Commit (#287)
    
    * Commit (#290)
    
    * Commit (#301)
    
    * Commit (#308)
    
    * Update README.md
    
    * Commit (#320)
    
    * Campaigns List for Users  (#325)
    
    * Commit
    
    * Commit
    
    * User campaign list  (#327)
    
    * Commit
    
    * Commit
    
    * Commit

--- src/user/campaignList/CampaignList.tsx
+++ src/user/campaignList/CampaignList.tsx
@@ -5,11 +5,16 @@ import * as _ from "lodash";
 import Section from "../../components/section/Section";
 
 import { Text } from "../../components/Text/Text";
-import { CAMPAIGN_LIST_OVERVIEW } from "./lib/CampaignList.queries";
+import { CAMPAIGN_LIST } from "./lib/CampaignList.queries";
 import { useQuery } from "@apollo/react-hooks";
 
 import Table from "../../components/Table/TableComponent";
 import { Link } from "react-router-dom";
+
+import { Icon } from "@material-ui/core";
+
+import * as S from "./style/CampaignList.style";
+
 
 
 
@@ -18,7 +23,9 @@ const CampaignList = props => {
 
     const { match } = props;
 
-    const { loading, error, data } = useQuery(CAMPAIGN_LIST_OVERVIEW, {});
+    const { loading, error, data } = useQuery(CAMPAIGN_LIST, {
+        variables: { id: props.advertiserId }
+    });
 
     const columns = [
         {
@@ -26,21 +33,41 @@ const CampaignList = props => {
             accessor: 'name',
             Cell: (props) => {
                 return (
-                    <Link style={{ textDecoration: "none", color: "rgb(251, 84, 43)", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }} to={`/admin/main/users/${props.row.original.advertiser.userAdvertisers[0].userId}/advertiser/${props.row.original.advertiser.id}/campaign/${props.row.original.id}`}>
-                        <div title={props.row.original.name}>{props.row.original.name}</div>
-                    </Link>)
+                    // <Link style={{ textDecoration: "none", color: "rgb(251, 84, 43)", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }} to={match.url}>
+                    <div style={{ textDecoration: "none", color: "rgb(251, 84, 43)", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }} title={props.row.original.name}>{props.row.original.name}</div>
+                    // </Link>)
+                )
             }
         },
         {
-            Header: 'Advertiser',
-            accessor: 'advertiser.name',
+            Header: 'Status',
+            accessor: 'state',
+            Cell: (props) => {
+                return props.row.original.state === "active" ? <div title="Active"><S.ActiveSymbol /></div> : <div title={props.row.original.state}><S.PendingSymbol /></div>
+            }
         },
         {
-            Header: 'Date Added',
-            accessor: 'createdAt',
+            Header: 'Budget',
+            accessor: 'budget',
             sortDescFirst: true,
             Cell: (props) => {
-                return new Date(props.row.original.createdAt).toLocaleDateString("en-US")
+                return props.row.original.currency === "USD" ? `$${props.row.original.budget.toFixed(2)}` : `${props.row.original.budget.toFixed(2)} BAT`
+            },
+        },
+        {
+            Header: 'Daily Budget',
+            accessor: 'dailyBudget',
+            sortDescFirst: true,
+            Cell: (props) => {
+                return props.row.original.currency === "USD" ? `$${props.row.original.dailyBudget.toFixed(2)}` : `${props.row.original.dailyBudget.toFixed(2)} BAT`
+            },
+        },
+        {
+            Header: 'Spend',
+            accessor: 'spent',
+            sortDescFirst: true,
+            Cell: (props) => {
+                return props.row.original.currency === "USD" ? `$${props.row.original.spent.toFixed(2)}` : `${props.row.original.spent.toFixed(2)} BAT`
             },
         },
         {
@@ -50,7 +77,21 @@ const CampaignList = props => {
             Cell: (props) => {
                 return new Date(props.row.original.startAt).toLocaleDateString("en-US")
             },
-        }
+        },
+        {
+            Header: 'End Date',
+            accessor: 'endAt',
+            sortDescFirst: true,
+            Cell: (props) => {
+                return new Date(props.row.original.startAt).toLocaleDateString("en-US")
+            },
+        },
+        {
+            accessor: 'currency',
+            Cell: (props) => {
+                return <div style={{ display: "flex", cursor: "pointer", color: "rgb(251, 84, 43)" }}><Icon title="Analytics">bar_chart</Icon></div>
+            },
+        },
     ];
 
     if (loading) return <></>;
@@ -60,8 +101,7 @@ const CampaignList = props => {
     return (
         <div>
             <Section fullWidthChild={true}>
-                Campaign List!
-                {/* <Table data={data.campaigns} columns={columns} tableWidth={1094} columnCount={5} /> */}
+                <Table data={data.advertiser.campaigns} columns={columns} tableWidth={1094} columnCount={8} />
             </Section>
         </div>
     );
