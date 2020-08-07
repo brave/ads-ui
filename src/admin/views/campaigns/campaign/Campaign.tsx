b78bf5f257628d4d884a2cfa41e793caa9f9d895
Dan A. Lipeles <danlipeles@icloud.com>
2020-08-07 14:58:09 -0400

    Add priorities 6-10

--- src/admin/views/campaigns/campaign/Campaign.tsx
+++ src/admin/views/campaigns/campaign/Campaign.tsx
@@ -53,6 +53,11 @@ const priorityTypes = [
     { value: 3, label: '3' },
     { value: 4, label: '4' },
     { value: 5, label: '5' },
+    { value: 6, label: '6' },
+    { value: 7, label: '7' },
+    { value: 8, label: '8' },
+    { value: 9, label: '9' },
+    { value: 10, label: '10' },
 ]
 
 const Campaign = props => {
