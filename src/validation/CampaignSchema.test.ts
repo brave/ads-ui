98fb2ab9a3b175ee35822251e90143f61f0775ce
Ian Krieger <iandkrieger@gmail.com>
2023-02-07 15:44:38 -0500

    add a test

---
+++ src/validation/CampaignSchema.test.ts
@@ -0,0 +1,39 @@
+import { parseISO } from "date-fns";
+import { produce } from "immer";
+import {CampaignFormat, CampaignPacingStrategies} from "../graphql/types";
+import {CampaignSchema} from "./CampaignSchema";
+
+const validCampaign = {
+  name: "some campaign",
+  budget: 1000,
+  currency: "GBP",
+  dailyBudget: 100,
+  dailyCap: 1,
+  dayProportion: 0.5,
+  startAt: parseISO("2030-07-18"),
+  endAt: parseISO("2030-07-20"),
+  format: CampaignFormat.PushNotification,
+  geoTargets: [{code: "a", name: "USA"}],
+  ntpSiScheduleIsValid: true,
+  state: "any",
+  type: "any",
+  externalId: "",
+  pacingStrategy: CampaignPacingStrategies.Original,
+  passThroughRate: 1,
+  priority: 1,
+  pacingOverride: true,
+};
+
+it("should pass on a valid object", () => {
+  CampaignSchema.validateSync(validCampaign);
+});
+
+it("should fail if the campaign start date is in past", () => {
+  const c = produce(validCampaign, (draft) => {
+    draft.startAt = parseISO("2020-07-18");
+  });
+
+  expect(() =>
+    CampaignSchema.validateSync(c)
+  ).toThrowErrorMatchingInlineSnapshot(`"Start Date must not be in the past"`);
+});
