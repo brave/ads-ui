25295167f7d68823b1f349e6d71b16e44a42667d
Ian Krieger <iandkrieger@gmail.com>
2023-04-05 15:36:13 -0400

    fix

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import advertiserReducer from "./advertiser";
 import authReducer from "./auth";
 import snackBarReducer from "./snackbar";
+import { reducer as formReducer } from "redux-form";
 
 export default combineReducers<any, any>({
   advertiserReducer,
   authReducer,
   snackBarReducer,
+  form: formReducer,
 });
