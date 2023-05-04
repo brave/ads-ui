60b0d75e3da401c06ad58a9ec3ebe6f7143906e1
Ian Krieger <48930920+IanKrieger@users.noreply.github.com>
2023-05-04 12:56:05 -0400

    secure link signin (#744)
    * login page styling phase one
    
    * tighten up login ui
    
    * update components
    
    * no need for status check
    
    * update hooks, do some route refactoring
    
    * no need for if statement
    
    * use link component

---
+++ src/auth/views/Login.tsx
@@ -0,0 +1,69 @@
+import { Alert, Link, TextField, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import React, { useState } from "react";
+import { useHistory, Link as RouterLink } from "react-router-dom";
+import { useSignIn } from "auth/hooks/mutations/useSignIn";
+import { AuthContainer } from "auth/views/components/AuthContainer";
+
+export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const { signIn, loading, error } = useSignIn({
+    onSuccess() {
+      history.replace("/");
+    },
+  });
+
+  return (
+    <AuthContainer>
+      <Typography sx={{ fontFamily: "Poppins", color: "#434251" }} variant="h4">
+        Sign into your brave Ads account
+      </Typography>
+      <TextField
+        sx={{ mt: 5, mb: 3 }}
+        fullWidth
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        label="Email"
+        placeholder="Enter your email"
+      />
+      <TextField
+        fullWidth
+        value={password}
+        type="password"
+        sx={{ mb: 2 }}
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        placeholder="Enter your password"
+      />
+
+      {error && <Alert severity="error">{error}</Alert>}
+
+      <LoadingButton
+        color="primary"
+        size="large"
+        variant="contained"
+        sx={{ mt: 2, textTransform: "none", mb: 1 }}
+        disabled={loading}
+        loading={loading}
+        onClick={() => {
+          signIn(email, password);
+        }}
+      >
+        Sign In
+      </LoadingButton>
+
+      <Link
+        underline="none"
+        component={RouterLink}
+        sx={{ mt: 1 }}
+        to="/auth/link"
+        replace
+      >
+        or sign in in using a secure link
+      </Link>
+    </AuthContainer>
+  );
+}
---
+++ src/auth/views/MagicLink.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+import { Link, Stack, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { useGetLink } from "auth/hooks/mutations/useGetLink";
+import { LoadingButton } from "@mui/lab";
+import { AuthContainer } from "auth/views/components/AuthContainer";
+
+export function MagicLink() {
+  const [email, setEmail] = useState("");
+  const [requested, setRequested] = useState(false);
+
+  const { requestLink, loading, error } = useGetLink({
+    onSuccess() {
+      setRequested(true);
+    },
+  });
+
+  if (requested) {
+    return (
+      <AuthContainer height="400px">
+        <Typography variant="h4" sx={{ mb: 3, mt: 2 }}>
+          A login email is on the way
+        </Typography>
+        <Typography variant="subtitle1" sx={{ mb: 2 }}>
+          Click on the secure login link in the email to access your brave Ads
+          account.
+        </Typography>
+        <Stack direction="row" spacing={0.75}>
+          <Typography variant="subtitle1">
+            Don't see the email? Check your spam folder or
+          </Typography>
+          <Link
+            sx={{ cursor: "pointer" }}
+            variant="subtitle1"
+            onClick={() => {
+              setRequested(false);
+            }}
+          >
+            try again.
+          </Link>
+        </Stack>
+      </AuthContainer>
+    );
+  }
+
+  return (
+    <AuthContainer height="400px">
+      <TextField
+        sx={{ mb: 2 }}
+        fullWidth
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        label="Email"
+        placeholder="Enter your email"
+        error={!!error}
+        helperText={error}
+      />
+      <Typography sx={{ textAlign: "center", mb: 1 }}>
+        Enter your email address to get a secure login link. Use this link to
+        access your brave Ads account.
+      </Typography>
+
+      <LoadingButton
+        color="primary"
+        size="large"
+        variant="contained"
+        sx={{ mt: 2, textTransform: "none", mb: 1 }}
+        disabled={loading}
+        loading={loading}
+        onClick={() => {
+          requestLink(email);
+        }}
+      >
+        Get login link
+      </LoadingButton>
+
+      <Link
+        underline="none"
+        sx={{ mt: 1 }}
+        component={RouterLink}
+        to="/auth/signin"
+        replace
+      >
+        or log in in using a password
+      </Link>
+    </AuthContainer>
+  );
+}
