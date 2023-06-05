5ce7eb01fc12c577ff3a408cee9241e0967913f1
Ian Krieger <iandkrieger@gmail.com>
2023-06-05 11:56:44 -0400

    completion view need to remain a card

--- src/user/views/adsManager/views/advanced/components/completionForm/CompletionForm.tsx
+++ src/user/views/adsManager/views/advanced/components/completionForm/CompletionForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import { useHistory, useParams } from "react-router-dom";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, Container, Stack, Typography } from "@mui/material";
 import present from "../../../../../../../../present.png";
 import { LoadingButton } from "@mui/lab";
 import { useValidatePaymentSession } from "checkout/hooks/useValidatePaymentSession";
@@ -16,77 +16,79 @@ export function CompletionForm() {
   const [clickedSurvey, setClickedSurvey] = useState(false);
 
   return (
-    <Box
-      sx={{
-        p: 2,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Box>
-        <img src={present} width="600px" style={{ marginRight: 50 }} />
-      </Box>
+    <Container maxWidth="xl">
+      <Card
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <img src={present} width="600px" style={{ marginRight: 50 }} />
+        </Box>
 
-      <Typography variant="h4" sx={{ textAlign: "center", mb: 3 }}>
-        Congratulations!
-      </Typography>
+        <Typography variant="h4" sx={{ textAlign: "center", mb: 3 }}>
+          Congratulations!
+        </Typography>
 
-      {params.mode === "edit" && (
-        <>
-          <Typography sx={{ textAlign: "center" }} variant="h6">
-            Your campaign has been updated and any new creatives will be
-            reviewed by our ads team. <br />
-            If you added new Ads, we'll be in contact as soon as they are
-            approved and activated. <br />
-            Thank you for using Brave Ads!
-          </Typography>
-
-          <Button
-            variant="contained"
-            size="large"
-            sx={{ mt: 2, flexGrow: 0 }}
-            onClick={() => history.push("/user/main/campaigns")}
-          >
-            Continue
-          </Button>
-        </>
-      )}
-
-      {params.mode === "new" && (
-        <>
-          <Typography sx={{ textAlign: "center" }} variant="h6">
-            Your campaign has been created and is now being reviewed by our ads
-            team. <br />
-            We'll be in contact as soon as your campaign is approved and
-            activated.
-          </Typography>
-
-          {!clickedSurvey && (
-            <Typography sx={{ textAlign: "center", mt: 3 }} variant="h6">
-              We value your feedback and would love to hear your thoughts on
-              your recent experience. <br />
-              Your input will help us improve the Brave Ads platform to better
-              meet your needs. <br />
-              It only takes a few minutes and your participation will be greatly
-              appreciated! <br />
+        {params.mode === "edit" && (
+          <>
+            <Typography sx={{ textAlign: "center" }} variant="h6">
+              Your campaign has been updated and any new creatives will be
+              reviewed by our ads team. <br />
+              If you added new Ads, we'll be in contact as soon as they are
+              approved and activated. <br />
+              Thank you for using Brave Ads!
             </Typography>
-          )}
 
-          <Stack
-            direction="row"
-            justifyContent="space-evenly"
-            sx={{ mt: 2 }}
-            spacing={2}
-          >
-            <ValidateCampaignButton
-              clicked={clickedSurvey}
-              onClick={() => setClickedSurvey(true)}
-            />
-          </Stack>
-        </>
-      )}
-    </Box>
+            <Button
+              variant="contained"
+              size="large"
+              sx={{ mt: 2, flexGrow: 0 }}
+              onClick={() => history.push("/user/main/campaigns")}
+            >
+              Continue
+            </Button>
+          </>
+        )}
+
+        {params.mode === "new" && (
+          <>
+            <Typography sx={{ textAlign: "center" }} variant="h6">
+              Your campaign has been created and is now being reviewed by our
+              ads team. <br />
+              We'll be in contact as soon as your campaign is approved and
+              activated.
+            </Typography>
+
+            {!clickedSurvey && (
+              <Typography sx={{ textAlign: "center", mt: 3 }} variant="h6">
+                We value your feedback and would love to hear your thoughts on
+                your recent experience. <br />
+                Your input will help us improve the Brave Ads platform to better
+                meet your needs. <br />
+                It only takes a few minutes and your participation will be
+                greatly appreciated! <br />
+              </Typography>
+            )}
+
+            <Stack
+              direction="row"
+              justifyContent="space-evenly"
+              sx={{ mt: 2 }}
+              spacing={2}
+            >
+              <ValidateCampaignButton
+                clicked={clickedSurvey}
+                onClick={() => setClickedSurvey(true)}
+              />
+            </Stack>
+          </>
+        )}
+      </Card>
+    </Container>
   );
 }
 
