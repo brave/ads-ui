36bf22ccd87d1d45d29a365f1bfefebc52aec01c
Ian Krieger <iandkrieger@gmail.com>
2023-06-14 12:36:06 -0400

    add contact email

--- src/user/components/navbar/Navbar.tsx
+++ src/user/components/navbar/Navbar.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
 
-import { Link, useHistory, useRouteMatch } from "react-router-dom";
+import {
+  Link as RouterLink,
+  useHistory,
+  useRouteMatch,
+} from "react-router-dom";
 
 import TopBarProgress from "react-topbar-progress-indicator";
 
-import { AppBar, Button, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Link,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { UserMenu } from "user/components/navbar/components/UserMenu";
 import { DraftMenu } from "user/components/navbar/components/DraftMenu";
 import moment from "moment";
@@ -42,10 +54,26 @@ export function Navbar() {
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, bgcolor: "#ffffff" }}
     >
       <Toolbar>
-        <Link style={logoStyle} to="/user/main">
+        <RouterLink style={logoStyle} to="/user/main">
           <img src={rewards} alt="Ads" height="40px" />
-        </Link>
-        {advertiser.selfServiceCreate && <DraftMenu />}
+        </RouterLink>
+        {advertiser.selfServiceCreate && (
+          <Stack direction="row" alignItems="center" spacing={2}>
+            <DraftMenu />
+            <div>
+              <Typography color="black">
+                Need Help?
+                <Link
+                  sx={{ ml: 1 }}
+                  href="mailto:selfserve@brave.com"
+                  underline="none"
+                >
+                  selfserve@brave.com
+                </Link>
+              </Typography>
+            </div>
+          </Stack>
+        )}
         <div style={{ flexGrow: 1 }} />
         {advertiser.selfServiceCreate && (
           <Button
