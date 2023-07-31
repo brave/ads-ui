52e51f03fdbb3f66965ce59faf0ca61651dc11ef
Ian Krieger <48930920+IanKrieger@users.noreply.github.com>
2023-07-31 08:50:55 -0400

    fix: allow campaign settings edit if draft (#841)

--- src/user/views/adsManager/views/advanced/components/campaign/CampaignSettings.tsx
+++ src/user/views/adsManager/views/advanced/components/campaign/CampaignSettings.tsx
@@ -4,8 +4,13 @@ import { CardContainer } from "components/Card/CardContainer";
 import { CampaignDateRange } from "components/Campaigns/CampaignDateRange";
 import { LocationField } from "user/views/adsManager/views/advanced/components/campaign/fields/LocationField";
 import { Typography } from "@mui/material";
+import { useFormikContext } from "formik";
+import { CampaignForm } from "user/views/adsManager/types";
 
 export function CampaignSettings(props: { isEdit: boolean }) {
+  const { values } = useFormikContext<CampaignForm>();
+  const isEdit = props.isEdit && values.state !== "draft";
+
   return (
     <>
       <CardContainer header="Campaign Settings">
@@ -15,10 +20,10 @@ export function CampaignSettings(props: { isEdit: boolean }) {
 
         <FormikTextField name="name" label="Campaign Name" sx={{ mb: 1 }} />
 
-        <CampaignDateRange isEdit={props.isEdit} />
+        <CampaignDateRange isEdit={isEdit} />
       </CardContainer>
 
-      {!props.isEdit && <LocationField />}
+      {!isEdit && <LocationField />}
     </>
   );
 }
