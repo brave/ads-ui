98db6405fd3cdc1c34ae745cc431cd0001a5937f
Ian Krieger <iandkrieger@gmail.com>
2023-11-06 08:40:35 -0500

    fix: hide segment config in news

--- src/user/views/adsManager/views/advanced/components/adSet/fields/PickerFields.tsx
+++ src/user/views/adsManager/views/advanced/components/adSet/fields/PickerFields.tsx
@@ -2,20 +2,26 @@ import { Typography } from "@mui/material";
 import { SegmentPicker } from "components/Segment/SegmentPicker";
 import { PlatformPicker } from "components/Platform/PlatformPicker";
 import { CardContainer } from "components/Card/CardContainer";
+import { useField } from "formik";
+import { CampaignFormat } from "graphql/types";
 
 interface Props {
   index: number;
 }
 
 export function PickerFields({ index }: Props) {
+  const [, format] = useField<CampaignFormat>("format");
+
   return (
     <>
-      <CardContainer header="Categories">
-        <Typography variant="body2" sx={{ mb: 2 }}>
-          Select the audience you would like to advertise to by interests.
-        </Typography>
-        <SegmentPicker idx={index} />
-      </CardContainer>
+      {format.value !== CampaignFormat.NewsDisplayAd && (
+        <CardContainer header="Categories">
+          <Typography variant="body2" sx={{ mb: 2 }}>
+            Select the audience you would like to advertise to by interests.
+          </Typography>
+          <SegmentPicker idx={index} />
+        </CardContainer>
+      )}
 
       <CardContainer header="Platforms">
         <Typography variant="body2" sx={{ mb: 2 }}>
