9cf9ef0d2401fddef84975a2b3ed9d64b3f7d53a
Ian Krieger <iandkrieger@gmail.com>
2023-11-22 10:41:06 -0500

    fix: change CPM long label

--- src/util/billingType.ts
+++ src/util/billingType.ts
@@ -5,7 +5,7 @@ interface BillingTypeLabels {
 }
 
 const BILLING_TYPES = [
-  { value: "cpm", shortLabel: "CPM", longLabel: "CPM (Views)" },
+  { value: "cpm", shortLabel: "CPM", longLabel: "CPM (Impressions)" },
   { value: "cpc", shortLabel: "CPC", longLabel: "CPC (Clicks)" },
   { value: "cpsv", shortLabel: "CPSV", longLabel: "CPSV (Site Visits)" },
 ];
