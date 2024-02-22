e0bb23f060376b4766298bbe7077d8f5b995df0b
Ian Krieger <48930920+IanKrieger@users.noreply.github.com>
2024-02-22 12:09:49 -0500

    fix: do not localize mui (#1099)

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,68 +1,64 @@
 import { createTheme, responsiveFontSizes, SxProps } from "@mui/material";
-import { esES } from "@mui/material/locale";
 
-const appTheme = createTheme(
-  {
-    typography: {
-      fontFamily: "Poppins",
-      h2: {
-        fontSize: "14px",
-        fontWeight: 600,
+const appTheme = createTheme({
+  typography: {
+    fontFamily: "Poppins",
+    h2: {
+      fontSize: "14px",
+      fontWeight: 600,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#423eee",
+    },
+    secondary: {
+      main: "#fe5907",
+    },
+    background: {
+      default: "#F1F3F5",
+      paper: "white",
+    },
+    text: {
+      primary: "#343546",
+      secondary: "#A0A1B2",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "1000px",
+          textTransform: "none",
+        },
       },
     },
-    palette: {
-      primary: {
-        main: "#423eee",
-      },
-      secondary: {
-        main: "#fe5907",
-      },
-      background: {
-        default: "#F1F3F5",
-        paper: "white",
-      },
-      text: {
-        primary: "#343546",
-        secondary: "#A0A1B2",
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: "12px",
+          boxShadow: "rgba(99, 105, 110, 0.18) 0px 1px 12px 0px",
+        },
       },
     },
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            borderRadius: "1000px",
-            textTransform: "none",
-          },
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          marginLeft: 0,
         },
       },
-      MuiCard: {
-        styleOverrides: {
-          root: {
-            borderRadius: "12px",
-            boxShadow: "rgba(99, 105, 110, 0.18) 0px 1px 12px 0px",
-          },
-        },
-      },
-      MuiContainer: {
-        styleOverrides: {
-          root: {
-            marginLeft: 0,
-          },
-        },
-      },
-      MuiAlert: {
-        styleOverrides: {
-          root: {
-            borderRadius: "16px",
-            mt: 1,
-            mb: 1,
-          },
+    },
+    MuiAlert: {
+      styleOverrides: {
+        root: {
+          borderRadius: "16px",
+          mt: 1,
+          mb: 1,
         },
       },
     },
   },
-  esES,
-);
+});
 
 export const theme = responsiveFontSizes(appTheme);
 
