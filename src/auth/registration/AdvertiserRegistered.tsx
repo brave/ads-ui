482c9f91669d06d61eda060ed81911850f2ba1df
Ian Krieger <iandkrieger@gmail.com>
2024-02-29 14:03:04 -0500

    feat: registration complete page

--- src/auth/registration/AdvertiserRegistered.tsx
+++ src/auth/registration/AdvertiserRegistered.tsx
@@ -2,39 +2,33 @@ import { Link, Stack, Typography } from "@mui/material";
 import { useTrackMatomoPageView } from "hooks/useTrackWithMatomo";
 import { Trans } from "@lingui/macro";
 import { AuthContainer } from "auth/views/components/AuthContainer";
+import logo from "../../../brave-icon-release-color.svg";
 
 export function AdvertiserRegistered() {
   useTrackMatomoPageView({ documentTitle: "Advertiser Registered" });
 
   return (
     <AuthContainer>
-      <Stack direction="column" alignItems="center">
-        <Typography variant="h5" sx={{ mb: 3 }}>
+      <Stack direction="column" alignItems="center" spacing={3}>
+        <img src={logo} height={60} />
+        <Typography variant="h4" sx={{ mb: 3 }}>
+          <Trans>Success! Check your email.</Trans>
+        </Typography>
+        <Typography variant="h5" sx={{ mb: 3 }} textAlign="center">
           <Trans>
-            Thanks for your interest in Brave Ads! Our team will now carefully
-            review the provided information.
+            You will soon receive an email with the next steps of registration.
+            While you wait, we recommend you check out our help center to get
+            helpful information for getting started with Brave Ads.
           </Trans>
         </Typography>
-        <Typography variant="h5" sx={{ mb: 3 }}>
-          <Trans>
-            Once the review process is complete, we will send you an email to
-            notify you of the approval status and any further steps required.
-          </Trans>
-        </Typography>
-        <Typography variant="h5" sx={{ textAlign: "left" }}>
-          <Trans>
-            In the meantime, check out our{" "}
-            <Link
-              href="https://brave.com/brave-ads"
-              variant="h5"
-              target="_blank"
-            >
-              advertiser resources
-            </Link>
-            . If you have any questions, please reach out to{" "}
-            <Link href="mailto:selfserve@brave.com">selfserve@brave.com</Link>.
-          </Trans>
-        </Typography>
+        <Link
+          href="https://ads-help.brave.com"
+          variant="h5"
+          target="_blank"
+          underline="none"
+        >
+          <Trans>Visit Help Center</Trans>
+        </Link>
       </Stack>
     </AuthContainer>
   );
