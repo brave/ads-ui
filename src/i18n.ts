46cd601eb38f425aa07dee6b4e2316e305d46737
Ian Krieger <iandkrieger@gmail.com>
2024-04-26 11:03:15 -0400

    feat: add image localization

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,31 @@
 import { detect, fromUrl } from "@lingui/detect-locale";
 import { enUS, esES, ptBR } from "@mui/x-data-grid/locales";
 
+import benefits from "@/assets/images/ad-benefits.svg";
+import benefitsEs from "@/assets/images/ad-benefits-es.svg";
+import benefitsPt from "@/assets/images/ad-benefits-pt.svg";
+
 const locales = [
-  { code: "en", name: "English", gridTheme: enUS },
-  { code: "es", name: "Español", gridTheme: esES },
-  { code: "pt", name: "Português", gridTheme: ptBR },
-  { code: "test", name: "Pseudolocale", gridTheme: enUS },
+  { code: "en", name: "English", gridTheme: enUS, images: { benefits } },
+  {
+    code: "es",
+    name: "Español",
+    gridTheme: esES,
+    images: { benefits: benefitsEs },
+  },
+  {
+    code: "pt",
+    name: "Português",
+    gridTheme: ptBR,
+    images: { benefits: benefitsPt },
+  },
+  { code: "test", name: "Pseudolocale", gridTheme: enUS, images: { benefits } },
 ];
 
 export function findLocale() {
   let locale = "en";
   let gridTheme = enUS;
+  let images = { benefits };
 
   const detectedLocale = detect(fromUrl("lang") /*fromNavigator(), */);
 
@@ -21,8 +36,9 @@ export function findLocale() {
     if (matchedLocale) {
       locale = matchedLocale.code;
       gridTheme = matchedLocale.gridTheme;
+      images = matchedLocale.images;
     }
   }
 
-  return { locale, gridTheme };
+  return { locale, gridTheme, images };
 }
