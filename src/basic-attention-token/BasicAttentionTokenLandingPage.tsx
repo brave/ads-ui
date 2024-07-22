85215c4cddd80e3f803e09d5222e821d3810ca96
Ian Krieger <iandkrieger@gmail.com>
2024-07-22 08:34:53 -0400

    fix(localization): add closing slash

--- src/basic-attention-token/BasicAttentionTokenLandingPage.tsx
+++ src/basic-attention-token/BasicAttentionTokenLandingPage.tsx
@@ -79,7 +79,7 @@ function LaunchCampaignButton() {
           trackMatomoEvent("basic-attention-token", "launch-campaign")
         }
         sx={{
-          width: "300px",
+          width: "250px",
           maxHeight: { xs: "40px", md: "60px" },
           mb: 1,
           fontSize: "16px",
