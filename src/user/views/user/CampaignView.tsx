a6ffcb0779ff4a10d803ec7a3d095527663af0ee
Graham Tackley <gtackley@brave.com>
2024-08-01 18:01:39 +0100

    chore: remove "conversion reporting" message (#1292)
    This was introduced nearly a month ago in #1249. It can now be removed
    as all that need to have seen it will have done so.

--- src/user/views/user/CampaignView.tsx
+++ src/user/views/user/CampaignView.tsx
@@ -11,7 +11,6 @@ import { useTrackMatomoPageView } from "@/hooks/useTrackWithMatomo";
 import { Trans, msg } from "@lingui/macro";
 import { AdvertiserCampaignsDocument } from "@/graphql-client/graphql";
 import { useQuery } from "@apollo/client";
-import { ConversionAlert } from "@/user/views/user/ConversionAlert";
 
 export function CampaignView() {
   useTrackMatomoPageView({ documentTitle: "Advertiser Campaigns" });
@@ -40,7 +39,6 @@ export function CampaignView() {
   return (
     <MiniSideBar>
       <Box display="flex" overflow="auto" flexDirection="column">
-        <ConversionAlert />
         <CardContainer
           header={<Trans>Campaigns</Trans>}
           sx={{
