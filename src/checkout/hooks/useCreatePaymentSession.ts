7365a934d1fc73c58a55c0406e288b059e955120
Ian Krieger <48930920+IanKrieger@users.noreply.github.com>
2024-08-21 16:50:58 -0400

    fix(payment): allow switching between payment methods (#1327)
    The way campaigns were updated has changed, we need to make sure if a
    person wants to change their payment method they can

--- src/checkout/hooks/useCreatePaymentSession.ts
+++ src/checkout/hooks/useCreatePaymentSession.ts
@@ -3,24 +3,28 @@ import { useCallback, useState } from "react";
 import { useAdvertiser } from "@/auth/hooks/queries/useAdvertiser";
 import { useHistory } from "react-router-dom";
 import { t } from "@lingui/macro";
+import { PaymentType } from "@/graphql-client/graphql";
 
 export function useCreatePaymentSession() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
   const { advertiser } = useAdvertiser();
-  const replaceSession = useCallback(async (campaignId: string) => {
-    setLoading(true);
-    await createPaymentSession(advertiser.id, campaignId ?? "")
-      .then((url) => {
-        window.location.replace(url);
-      })
-      .catch(() => {
-        alert(t`Unable to create payment session. Please try again.`);
-        setLoading(false);
-        history.push(`/user/main/adsmanager/advanced/${campaignId}/review`);
-      });
-  }, []);
+  const replaceSession = useCallback(
+    async (campaignId: string, paymentMethod?: PaymentType) => {
+      setLoading(true);
+      await createPaymentSession(advertiser.id, campaignId ?? "", paymentMethod)
+        .then((url) => {
+          window.location.replace(url);
+        })
+        .catch(() => {
+          alert(t`Unable to create payment session. Please try again.`);
+          setLoading(false);
+          history.push(`/user/main/adsmanager/advanced/${campaignId}/review`);
+        });
+    },
+    [],
+  );
 
   return { createPaymentSession: replaceSession, loading };
 }
