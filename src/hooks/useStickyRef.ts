f148a169d0d2ca083af912efc571c83aaf3c6f9f
Ian Krieger <iandkrieger@gmail.com>
2024-09-20 16:09:46 -0400

    fix: ref

--- src/hooks/useStickyRef.ts
+++ src/hooks/useStickyRef.ts
@@ -1,8 +1,5 @@
 import { Dispatch, useCallback, useRef } from "react";
 
-// this is like "useState", but persists and restores the state to the local storage key
-// specified.  For simplicity, it doesn't fully support setting the state with a function,
-// as we can't always see the resulting value after the function is called.
 export function useStickyRef<S>(
   localStorageKey: string,
   initialState: S,
