455c003a3bf9b867ea5deb297deb4d9ba62e6cd1
Ian Krieger <48930920+IanKrieger@users.noreply.github.com>
2024-10-07 08:33:48 -0400

    fix: account balance wording (#1365)
    Better wording

--- src/user/views/adsManager/views/advanced/components/campaign/fields/PaymentMethodField.tsx
+++ src/user/views/adsManager/views/advanced/components/campaign/fields/PaymentMethodField.tsx
@@ -32,7 +32,7 @@ export function PaymentMethodField() {
   );
 
   const chargeMessage = (b: BigNumber) => {
-    if (b.lte(0)) return t`no additional charge will be applied.`;
+    if (b.lte(0)) return t`and no additional charges will be incurred.`;
     const amountToCharge = formatUsd(b);
     return t`and you will be charged ${amountToCharge} USD.`;
   };
@@ -59,8 +59,8 @@ export function PaymentMethodField() {
             <Trans>Account Balance will be applied to this campaign</Trans>
           </AlertTitle>
           <Trans>
-            Your account has a balance of ${formatUsd(balance)} USD. This will
-            be applied to your campaign
+            Your account has a balance of {formatUsd(balance)} USD. This will be
+            applied to your campaign
           </Trans>
           {", "}
           {chargeMessage(amountOwed(balance))}
