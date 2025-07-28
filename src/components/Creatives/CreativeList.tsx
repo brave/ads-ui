brave/ads-ui

commit 9001e1b190c9b5f4ae7623317810e8e058a519e0

feat: remove ability to create newsfeed campaigns (#1507)
Since the newsfeed is [transitioning to ad
free](https://community.brave.com/t/ad-free-brave-news-and-brave-rewards/621912)
remove the ability to create self-service newsfeed ads.

Also remove all the associated logic with uploading and creating the
associated images.

src/components/Creatives/CreativeList.tsx
@@ -9,17 +9,17 @@ import { Link as RouterLink } from "react-router-dom";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { CreativeStatusSwitch } from "@/components/Creatives/CreativeStatusSwitch";
 import { CustomToolbar } from "@/components/Datagrid/CustomToolbar";
-import { RouteSelectionButton } from "@/components/Route/RouteSelectionButton";
 import { useTrackMatomoPageView } from "@/hooks/useTrackWithMatomo";
-import { msg } from "@lingui/macro";
-import { Trans, useLingui } from "@lingui/react";
+import { useLingui, Trans as ReactTrans } from "@lingui/react";
+import { Trans, msg } from "@lingui/macro";
+
 import { MessageDescriptor } from "@lingui/core";
 import { useQuery } from "@apollo/client";
 import {
   AdvertiserCreativesDocument,
   CreativeFragment,
 } from "@/graphql-client/graphql";
-const ALLOWED_TYPES = ["notification_all_v1", "inline_content_all_v1"];
+const ALLOWED_TYPES = ["notification_all_v1"];
 
 export function CreativeList() {
   useTrackMatomoPageView({ documentTitle: "Advertiser Creatives" });
@@ -96,20 +96,10 @@ export function CreativeList() {
         />
       )}
       <CardContainer
-        header={
-          <RouteSelectionButton
-            routes={[
-              { label: msg`Ads`, value: "ads" },
-              { label: msg`Images`, value: "assets" },
-            ]}
-          />
-        }
-        useTypography={false}
+        header={<Trans>Ads</Trans>}
         sx={{
           flexGrow: 1,
           overflowX: "auto",
-          mr: 1,
-          mt: 0,
         }}
       >
         <Box>
@@ -157,19 +147,6 @@ function CreativePayloadList(props: { creative: CreativeFragment }) {
         />
       );
       break;
-    case "inline_content_all_v1":
-      listItems = (
-        <ListItems
-          items={[
-            { primary: msg`Title`, secondary: c.payloadInlineContent?.title },
-            {
-              primary: msg`Call To Action`,
-              secondary: c.payloadInlineContent?.ctaText,
-            },
-          ]}
-        />
-      );
-      break;
     default:
       listItems = null;
   }
@@ -192,7 +169,7 @@ const ListItems = (props: {
         paddingRight={1}
         fontWeight={600}
       >
-        <Trans id={i.primary.id} />
+        <ReactTrans id={i.primary.id} />
       </Typography>
       <Typography variant="body1" component="span">
         {i.secondary}

.husky/pre-commit
@@ -1,4 +1,1 @@
-#!/bin/sh
-. "$(dirname "$0")/_/husky.sh"
-
 npx lint-staged

src/auth/views/components/WhatIsBraveAds.tsx
@@ -18,8 +18,7 @@ export function WhatIsBraveAds() {
           <Trans>
             It is a modern <em>advertiser-first</em> ad platform made to help
             brands reach new users. Brave Ads are discrete, unobtrusive
-            first-party ads built directly into the browser and search engine,
-            with familiar formats like push notifications and news-feed ads.
+            first-party ads built directly into the browser and search engine.
             Brave’s innovative on-device targeting respects a user’s desire for
             privacy while still delivering personalized ads based on their
             interests, which drives high ROI for advertisers.

src/basic-attention-token/BasicAttentionTokenLandingPage.tsx
@@ -61,7 +61,7 @@ export function BasicAttentionTokenLandingPage({ reroute }: Props) {
             <Trans>
               *Limited time only. Available to new advertisers, and those who
               ran campaigns prior to September 30, 2023. Eligible for Push
-              Notification and Newsfeed ads.
+              Notification ads.
             </Trans>
           </Typography>
 

src/components/Assets/AdvertiserAssets.tsx
@@ -1,80 +1,0 @@
-import { useAdvertiser } from "@/auth/hooks/queries/useAdvertiser";
-import { ErrorDetail } from "@/components/Error/ErrorDetail";
-import { CardContainer } from "@/components/Card/CardContainer";
-import { Grid, LinearProgress, Typography } from "@mui/material";
-import MiniSideBar from "@/components/Drawer/MiniSideBar";
-import { ImagePreview } from "@/components/Assets/ImagePreview";
-import {
-  AdvertiserImageFragment,
-  AdvertiserImagesDocument,
-  CampaignFormat,
-} from "@/graphql-client/graphql";
-import { RouteSelectionButton } from "@/components/Route/RouteSelectionButton";
-import Box from "@mui/material/Box";
-import { useTrackMatomoPageView } from "@/hooks/useTrackWithMatomo";
-import { msg, Trans } from "@lingui/macro";
-import dayjs from "dayjs";
-import { useQuery } from "@apollo/client";
-
-export function AdvertiserAssets() {
-  useTrackMatomoPageView({ documentTitle: "Advertiser Assets" });
-  const { advertiser } = useAdvertiser();
-  const { data, loading, error } = useQuery(AdvertiserImagesDocument, {
-    variables: { id: advertiser.id },
-    initialFetchPolicy: "cache-and-network",
-    nextFetchPolicy: "cache-only",
-  });
-
-  return (
-    <MiniSideBar>
-      <Box display="flex" flexDirection="column" flexGrow={1}>
-        <RouteSelectionButton
-          routes={[
-            { label: msg`Ads`, value: "ads" },
-            { label: msg`Images`, value: "ads/assets" },
-          ]}
-        />
-        {loading && <LinearProgress sx={{ mt: 1, flexGrow: 1 }} />}
-        {error && (
-          <ErrorDetail
-            error={error}
-            additionalDetails={msg`Unable to retrieve images`}
-          />
-        )}
-        {!loading && !error && (
-          <Grid container spacing={2}>
-            {[...(data?.advertiser?.images ?? [])]
-              .sort(
-                (a, b) => dayjs(b.createdAt).date() - dayjs(a.createdAt).date(),
-              )
-              .map((i, idx) => (
-                <Grid sx={{ width: "auto" }} key={idx}>
-                  <GalleryItem image={i} />
-                </Grid>
-              ))}
-          </Grid>
-        )}
-      </Box>
-    </MiniSideBar>
-  );
-}
-
-const GalleryItem = (props: { image: AdvertiserImageFragment }) => {
-  const { name, imageUrl, createdAt, format } = props.image;
-  const height = format === CampaignFormat.NewsDisplayAd ? 400 : undefined;
-  const width = format === CampaignFormat.NewsDisplayAd ? 500 : undefined;
-
-  return (
-    <CardContainer header={name}>
-      <ImagePreview url={imageUrl} height={height} width={width} />
-      <Typography
-        variant="caption"
-        color="text.primary"
-        textAlign="right"
-        fontWeight={500}
-      >
-        <Trans>created</Trans> {dayjs(createdAt).fromNow()}
-      </Typography>
-    </CardContainer>
-  );
-};

src/components/Assets/ImageAutocomplete.tsx
@@ -1,78 +1,0 @@
-import { useAdvertiser } from "@/auth/hooks/queries/useAdvertiser";
-import { Autocomplete, createFilterOptions, TextField } from "@mui/material";
-import { useEffect, useState } from "react";
-import { useField } from "formik";
-import { UploadImage } from "@/components/Assets/UploadImage";
-import { useLingui } from "@lingui/react";
-import { msg } from "@lingui/macro";
-import { useQuery } from "@apollo/client";
-import { AdvertiserImagesDocument } from "@/graphql-client/graphql";
-
-type ImageOption = { label: string; image?: string };
-
-const filter = createFilterOptions<ImageOption>();
-
-export function ImageAutocomplete(props: { name: string }) {
-  const [createImage, setCreateImage] = useState(false);
-  const [, meta, imageUrl] = useField<string | undefined>(props.name);
-  const hasError = Boolean(meta.error);
-  const showError = hasError && meta.touched;
-  const { advertiser } = useAdvertiser();
-  const [options, setOptions] = useState<ImageOption[]>();
-  const { data, loading } = useQuery(AdvertiserImagesDocument, {
-    variables: { id: advertiser.id },
-  });
-  const { _ } = useLingui();
-
-  useEffect(() => {
-    const images = data?.advertiser?.images ?? [];
-    const options = images.map((i) => ({
-      label: i.name,
-      image: i.imageUrl,
-    }));
-
-    setOptions(options);
-  }, [data?.advertiser?.images]);
-
-  return (
-    <>
-      <Autocomplete
-        disablePortal
-        loading={!options || loading}
-        options={options ?? []}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label={_(msg`Image`)}
-            helperText={showError ? meta.error : undefined}
-            error={showError}
-            margin="normal"
-          />
-        )}
-        value={{
-          label: options?.find((o) => o.image === meta.value)?.label ?? "",
-          image: meta.value,
-        }}
-        getOptionLabel={(o) => o.label}
-        filterOptions={(options, params) => {
-          const filtered = filter(options, params);
-          return [
-            ...filtered,
-            { image: undefined, label: _(msg`Upload new image`) },
-          ];
-        }}
-        onChange={async (e, nv) => {
-          await imageUrl.setValue(nv ? nv.image : undefined);
-          setCreateImage(nv != null && nv.image === undefined);
-        }}
-        isOptionEqualToValue={(option, value) => option.image === value.image}
-      />
-
-      <UploadImage
-        useInlineCreation={createImage}
-        onClose={() => setCreateImage(false)}
-        onComplete={(url) => imageUrl.setValue(url)}
-      />
-    </>
-  );
-}

src/components/Assets/ImagePreview.tsx
@@ -1,64 +1,0 @@
-import { Box, Link, Skeleton } from "@mui/material";
-import { useGetImagePreviewUrl } from "@/components/Assets/hooks/useGetImagePreviewUrl";
-import { ErrorDetail } from "@/components/Error/ErrorDetail";
-import { useLingui } from "@lingui/react";
-import { msg } from "@lingui/macro";
-
-interface Props {
-  url: string;
-  height?: number;
-  width?: number;
-  selected?: boolean;
-}
-
-export const ImagePreview = ({
-  url,
-  height = 300,
-  width = 360,
-  selected,
-}: Props) => {
-  const { data, loading, error } = useGetImagePreviewUrl({ url });
-  const { _ } = useLingui();
-
-  if (!data || loading) {
-    return <Skeleton variant="rectangular" height={height} width={width} />;
-  }
-
-  if (error) {
-    return <ErrorDetail error={error} />;
-  }
-
-  return (
-    <Box
-      sx={{
-        height,
-        width,
-        bgcolor: "#AEB1C2",
-        borderRadius: "6px",
-      }}
-    >
-      {url?.endsWith(".pad") ? (
-        <img
-          height={height}
-          width={width}
-          src={data}
-          alt="preview"
-          style={{
-            opacity: selected === false ? 0.3 : 1,
-            filter: selected === false ? "grayscale(20%)" : "none",
-            borderRadius: "6px",
-          }}
-        />
-      ) : (
-        <Link underline="none" href={url} target="_blank" rel="noreferrer">
-          <img
-            height={height}
-            width={width}
-            src={data}
-            alt={_(msg`Image failed to load`)}
-          />
-        </Link>
-      )}
-    </Box>
-  );
-};

src/components/Assets/UploadImage.tsx
@@ -1,145 +1,0 @@
-import { useEffect, useState } from "react";
-import {
-  Alert,
-  Box,
-  Button,
-  Chip,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  LinearProgress,
-  Step,
-  StepLabel,
-  Stepper,
-} from "@mui/material";
-import { CampaignFormat } from "@/graphql-client/graphql";
-import { useUploadFile } from "@/components/Assets/hooks/useUploadFile";
-import { NewImageButton } from "@/components/Navigation/NewImageButton";
-import { Trans } from "@lingui/macro";
-
-export interface UploadConfig {
-  targetHost: () => string;
-  requiresPublishStep: boolean;
-  endpoint: string;
-}
-
-interface Props {
-  useInlineCreation?: boolean;
-  onComplete?: (url: string) => void;
-  onClose?: () => void;
-}
-
-export function UploadImage({ useInlineCreation, onClose, onComplete }: Props) {
-  const [open, setOpen] = useState(false);
-  const [file, setFile] = useState<File>();
-  const [{ upload, reset }, { step, error, loading, state }] = useUploadFile({
-    onComplete(data) {
-      if (useInlineCreation && onComplete) {
-        onComplete(data);
-      }
-    },
-  });
-
-  useEffect(() => {
-    if (useInlineCreation) {
-      setOpen(true);
-    }
-  }, [useInlineCreation]);
-
-  return (
-    <>
-      {useInlineCreation === undefined && (
-        <NewImageButton onClick={() => setOpen(true)} />
-      )}
-      <Dialog open={open} onClose={() => setOpen(false)}>
-        <DialogTitle>
-          <Trans>Upload Image</Trans>
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            <Trans>
-              Uploaded images can be shared across different ad sets within a
-              Campaign. For best quality, upload images at 900x750px resolution.
-              Images will be automatically scaled to this size.
-            </Trans>
-          </DialogContentText>
-
-          <Stepper activeStep={step} sx={{ mt: 3 }}>
-            <Step>
-              <StepLabel>
-                <Trans>Choose</Trans>
-              </StepLabel>
-            </Step>
-            <Step>
-              <StepLabel>
-                <Trans>Upload</Trans>
-              </StepLabel>
-            </Step>
-            <Step>
-              <StepLabel>
-                <Trans>Complete</Trans>
-              </StepLabel>
-            </Step>
-          </Stepper>
-
-          <Box mt={3}>
-            {step === 0 && file === undefined && (
-              <Button
-                variant="contained"
-                component="label"
-                sx={{ marginBottom: 1 }}
-              >
-                <Trans>Choose File</Trans>
-                <input
-                  type="file"
-                  hidden
-                  accept=".jpg, .jpeg, .png, .gif"
-                  onChange={(e) => setFile(e.target?.files?.[0])}
-                />
-              </Button>
-            )}
-            {step === 0 && !!file && (
-              <Chip
-                onDelete={() => setFile(undefined)}
-                label={file.name}
-                sx={{ marginBottom: 1 }}
-              />
-            )}
-
-            {!error && state && (
-              <Alert severity={step !== 2 ? "info" : "success"}>{state}</Alert>
-            )}
-            {error !== undefined && <Alert severity="error">{error}</Alert>}
-            {loading && <LinearProgress sx={{ mt: 1 }} />}
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => {
-              setOpen(false);
-              setFile(undefined);
-              reset!();
-              if (onClose) onClose();
-            }}
-            variant="outlined"
-          >
-            {step === 2 ? <Trans>Close</Trans> : <Trans>Cancel</Trans>}
-          </Button>
-          {step !== 2 && (
-            <Button
-              disabled={file === undefined || step !== 0}
-              onClick={() => {
-                upload!(file!, CampaignFormat.NewsDisplayAd);
-              }}
-              variant="contained"
-            >
-              <Trans>Upload</Trans>
-            </Button>
-          )}
-        </DialogActions>
-      </Dialog>
-    </>
-  );
-}

commit 048d9ce947706ed0e9c6bf13e9588eec9706487b

fix: `user/main/ads` fails with error
The complexity of translations hit us again, though this has apparently been broken since https://github.com/brave/ads-ui/pull/1507.

It seems the `Trans` macro doesn't apply the translations reliably, so use `_` instead.

src/components/Creatives/CreativeList.tsx
@@ -11,7 +11,7 @@ import { CreativeStatusSwitch } from "@/components/Creatives/CreativeStatusSwitc
 import { CustomToolbar } from "@/components/Datagrid/CustomToolbar";
 import { useTrackMatomoPageView } from "@/hooks/useTrackWithMatomo";
 import { useLingui, Trans as ReactTrans } from "@lingui/react";
-import { Trans, msg } from "@lingui/macro";
+import { msg } from "@lingui/macro";
 
 import { MessageDescriptor } from "@lingui/core";
 import { useQuery } from "@apollo/client";
@@ -96,7 +96,7 @@ export function CreativeList() {
         />
       )}
       <CardContainer
-        header={<Trans>Ads</Trans>}
+        header={_(msg`Ads`)}
         sx={{
           flexGrow: 1,
           overflowX: "auto",