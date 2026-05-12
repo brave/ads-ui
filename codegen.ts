6a8f57bba7b992c4e9023223d8da2cdd9f46ee8b
George Petrou <gpetrou@brave.com>
2026-05-12 15:43:54 +0300

    updated

--- codegen.ts
+++ codegen.ts
@@ -19,6 +19,7 @@ const config: CodegenConfig = {
   config: {
     strictScalars: true,
     useTypeImports: true,
+    enumType: "native",
     scalars: {
       DateTime: "string",
       Numeric: {
